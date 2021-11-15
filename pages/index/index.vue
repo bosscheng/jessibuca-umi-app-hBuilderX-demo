6b08218464d97ae030ebcb334d96f72225a0495a
bosscheng <25022549@qq.com>
2021-11-15 23:12:23 +0800

    add files

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<button type="default" @click="toLink()">进入播放器</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+
+		methods: {
+			toLink(link){
+				uni.navigateTo({
+				    url: '/pages/live/live'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
